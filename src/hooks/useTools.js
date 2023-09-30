aeaa4843c08a5a89cc36852fb799d84c3b4e1767
Sajeda Saju <sajedasaju85@gmail.com>
2023-09-30 22:23:07 +0600

    use tool change

--- src/hooks/useTools.js
+++ src/hooks/useTools.js
@@ -3,24 +3,18 @@ import { useEffect, useState } from "react";
 const useTools = () => {
   const [tools, setTools] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // console.log(isLoading)
   useEffect(() => {
-    // setIsLoading(true);
     try {
-      console.log("hello from usetool");
-      setIsLoading(true);
       fetch("https://the-cycle-gear-server-side-project.vercel.app/tool")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           setTools(data);
           setIsLoading(false);
-          console.log("vitor", isLoading);
         });
     } catch (e) {
       console.log("err", e);
     }
-  }, [isLoading]);
+  }, []);
   return [tools, setTools, isLoading];
 };
 export default useTools;
